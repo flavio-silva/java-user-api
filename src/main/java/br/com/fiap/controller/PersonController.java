17beab6db8d6e39f0a0d3c101742a02c3bf761f1
wellington bastos <wellingtonbastos@hotmail.com>
2019-10-20 21:49:58 -0300

    refactor controller

--- src/main/java/br/com/fiap/controller/PersonController.java
+++ src/main/java/br/com/fiap/controller/PersonController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
@@ -17,42 +18,30 @@ public class PersonController {
     @Autowired
     private PersonServices services;
 
-    @RequestMapping(method=RequestMethod.GET,
-            produces= MediaType.APPLICATION_JSON_VALUE)
-
+    @GetMapping
     public List<Person> findAll() {
         return services.findAll();
     }
 
-    @RequestMapping(value="/{id}",
-            method=RequestMethod.GET,
-            produces= MediaType.APPLICATION_JSON_VALUE)
-
+    @GetMapping("/{id}")
     public Person findById(@PathVariable("id") Long id ) {
         return services.findById(id);
     }
 
-    @RequestMapping(method=RequestMethod.POST,
-            consumes=MediaType.APPLICATION_JSON_VALUE,
-            produces=MediaType.APPLICATION_JSON_VALUE)
-
+    @PostMapping
     public Person create(@RequestBody Person person) {
         return services.create(person);
     }
 
-    @RequestMapping(method=RequestMethod.PUT,
-            consumes=MediaType.APPLICATION_JSON_VALUE,
-            produces=MediaType.APPLICATION_JSON_VALUE)
-
+    @PutMapping
     public Person update(@RequestBody Person person) {
         return services.update(person);
     }
 
-    @RequestMapping(value="/{id}",
-            method=RequestMethod.DELETE)
-
-    public void delete(@PathVariable("id") Long id ) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id ) {
         services.delete(id);
+        return ResponseEntity.ok().build();
     }
 
 
