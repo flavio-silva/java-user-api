5ff6b450dd00ed1826b2fc40fc52041b75cdfcc5
wellington bastos <wellingtonbastos@hotmail.com>
2019-10-29 22:31:58 -0300

    add swagger & and cors

--- services/java-user-api/src/main/java/br/com/fiap/controller/PersonController.java
+++ services/java-user-api/src/main/java/br/com/fiap/controller/PersonController.java
@@ -3,6 +3,8 @@ package br.com.fiap.controller;
 
 import br.com.fiap.model.Person;
 import br.com.fiap.services.PersonServices;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-
+@Api(value = "Person Endpoint", tags = {"PersonEndpoint"})
 @RestController
 @RequestMapping("/api/person/v1")
 public class PersonController {
@@ -18,26 +20,31 @@ public class PersonController {
     @Autowired
     private PersonServices services;
 
+    @ApiOperation(value = "Find All People Recorded.")
     @GetMapping
     public List<Person> findAll() {
         return services.findAll();
     }
 
+    @ApiOperation(value = "Find People By your Id.")
     @GetMapping("/{id}")
     public Person findById(@PathVariable("id") Long id ) {
         return services.findById(id);
     }
 
+    @ApiOperation(value = "Create a People.")
     @PostMapping
     public Person create(@RequestBody Person person) {
         return services.create(person);
     }
 
+    @ApiOperation(value = "Update People By your Id.")
     @PutMapping
     public Person update(@RequestBody Person person) {
         return services.update(person);
     }
 
+    @ApiOperation(value = "Delete People by your Id.")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") Long id ) {
         services.delete(id);
