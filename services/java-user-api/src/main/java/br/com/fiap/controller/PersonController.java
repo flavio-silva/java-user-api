a104b8af2ce71d0ccca25d2f5e42a3054b360038
wellington bastos <wellingtonbastos@hotmail.com>
2019-10-30 05:32:13 -0300

    add http  response

--- services/java-user-api/src/main/java/br/com/fiap/controller/PersonController.java
+++ services/java-user-api/src/main/java/br/com/fiap/controller/PersonController.java
@@ -6,6 +6,7 @@ import br.com.fiap.services.PersonServices;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,30 +22,35 @@ public class PersonController {
     private PersonServices services;
 
     @ApiOperation(value = "Find All People Recorded.")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<Person> findAll() {
         return services.findAll();
     }
 
     @ApiOperation(value = "Find People By your Id.")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
     public Person findById(@PathVariable("id") Long id ) {
         return services.findById(id);
     }
 
     @ApiOperation(value = "Create a People.")
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping
     public Person create(@RequestBody Person person) {
         return services.create(person);
     }
 
     @ApiOperation(value = "Update People By your Id.")
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping
     public Person update(@RequestBody Person person) {
         return services.update(person);
     }
 
     @ApiOperation(value = "Delete People by your Id.")
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") Long id ) {
         services.delete(id);
