c75e3f9477cf1373f4c8c28f16cfd8b1b6901803
Flavio Silva <flv.alves@gmail.com>
2019-10-31 12:13:45 -0300

    config webhook url from Slack

--- services/php-slack-consumer/app/User.php
+++ services/php-slack-consumer/app/User.php
@@ -2,7 +2,6 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -36,4 +35,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function routeNotificationForSlack($notification)
+    {
+        return 'https://hooks.slack.com/services/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';
+    }
 }
