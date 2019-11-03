f2a20e2a2d08de6c4853330c628639df733cb6a3
Flavio Silva <flv.alves@gmail.com>
2019-11-03 17:37:38 -0300

    user crud

--- services/front-end/src/App.vue
+++ services/front-end/src/App.vue
@@ -1,28 +1,309 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="container mt-5">
+      <h1 class="text-center h2">Cadastro de usuários</h1>
+
+      <form
+        class="mt-5"
+        v-on:submit.prevent="save(user)"
+      >
+
+        <div class="form-row">
+          <div class="form-group col-lg-6">
+            <label
+              for="first_name"
+              class="col-form-label font-weight-bold"
+            >Nome: <span class="text-danger">*</span></label>
+            <input
+              name="first_name"
+              id="first_name"
+              type="text"
+              class="form-control"
+              placeholder="Digite o seu nome"
+              required
+              v-model="user.first_name"
+            />
+          </div>
+
+          <div class="form-group col-lg-6">
+            <label
+              for="last_name"
+              class="col-form-label font-weight-bold"
+            >Sobrenome: <span class="text-danger">*</span></label>
+            <input
+              type="text"
+              name="last_name"
+              id="last_name"
+              class="form-control"
+              placeholder="Digite seu sobrenome"
+              required
+              v-model="user.last_name"
+            />
+          </div>
+        </div>
+
+        <div class="form-row">
+          <div class="form-group col-lg-8">
+            <label
+              for="email"
+              class="font-weight-bold col-form-label"
+            >Email: <span class="text-danger">*</span></label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              class="form-control"
+              placeholder="meuemail@email.com.br"
+              required
+              v-model="user.email"
+            >
+          </div>
+
+          <div class="form-group col-lg-4">
+            <label
+              for="gender"
+              class="font-weight-bold col-form-label"
+            >Sexo: <span class="text-danger">*</span></label>
+            <select
+              name="gender"
+              id="gender"
+              class="form-control"
+              required
+              v-model="user.gender"
+            >
+              <option
+                value=""
+                selected
+              >Selecione o sexo</option>
+              <option value="m">Masculino</option>
+              <option value="f">Feminino</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="form-row">
+          <div class="form-group col-lg-9">
+            <label
+              for="address"
+              class="font-weight-bold col-form-label"
+            >Endereço: <span class="text-danger">*</span></label>
+            <input
+              type="text"
+              name="address"
+              id="address"
+              placeholder="Digite o seu endereço"
+              class="form-control"
+              required
+              v-model="user.address"
+            >
+          </div>
+          <div class="form-group col-lg-3">
+            <label
+              for="phone"
+              class="font-weight-bold col-form-label"
+            >Telefone: </label>
+            <input
+              type="text"
+              name="phone"
+              id="phone"
+              class="form-control"
+              placeholder="(XX) X-XXXX-XXXX"
+              v-model="user.phone"
+            >
+          </div>
+        </div>
+
+        <div class="form-row">
+          <div class="form-group col-lg-6">
+            <label
+              for="username"
+              class="font-weight-bold col-form-label"
+            >Username: <span class="text-danger">*</span></label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Sem espaços e caracteres especiais"
+              class="form-control"
+              required
+              v-model="user.username"
+            >
+          </div>
+
+          <div class="form-group col-lg-6">
+            <label
+              for="password"
+              class="font-weight-bold col-form-label"
+            >
+              Senha: <span class="text-danger">*</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              class="form-control"
+              :placeholder="user.id ? 'Deixa em branco para a senha atual' : 'min 6 caracteres'"
+              minlength="6"
+              maxlength="80"
+              :required="!user.id"
+              v-model="user.password"
+            />
+          </div>
+        </div>
+        <button
+          type="submit"
+          class="btn btn-primary"
+          :disabled=loading
+        >
+          <span
+            class="fa"
+            :class="{ 'fa-save' : !loading, 'fa-spinner': loading, 'fa-spin': loading}"
+          ></span>
+          &nbsp; Salvar</button>
+
+      </form>
+
+      <h2 class="text-center my-5">Listagem de usuários</h2>
+
+      <table
+        class="table table-hover table-responsive"
+        v-if="users.length"
+      >
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Sobrenome</th>
+            <th>Email</th>
+            <th>Sexo</th>
+            <th>Endereço</th>
+            <th>Telefone</th>
+            <th>Username</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="user in users"
+            :key="user.id"
+          >
+            <td>{{ user.first_name }} </td>
+            <td>{{ user.last_name }} </td>
+            <td>{{ user.email }} </td>
+            <td>{{ user.gender === 'm' ? 'Masculino' : 'Feminino' }} </td>
+            <td>{{ user.address }} </td>
+            <td>{{ user.phone }} </td>
+            <td>{{ user.username }} </td>
+            <td class="d-flex justify-content-between">
+              <a
+                href="#"
+                v-on:click.prevent="fillUserInfo(user)"
+                class="fa fa-edit"
+              ></a>
+
+              <a
+                href="#"
+                v-on:click.prevent="deleteUser(user.id)"
+                class="fa fa-trash text-danger"
+              ></a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <p
+        v-else
+        class="text-center"
+      >
+        Nenhum usuário cadastrado
+      </p>
+    </div>
+    <footer class="bg-dark mt-5">
+      <p class="text-center text-white p-5 mb-0">
+        Todos os direitos reservados &copy;
+      </p>
+
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.css'
+import api from './services/api';
+import Swal from 'sweetalert2';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+
+  created () {
+    api.get().then(response => {
+      this.users = response.data;
+    })
+  },
+
+  data () {
+    return {
+      loading: false,
+      user: { gender: '' },
+      users: [],
+    }
+  },
+  methods: {
+    async save (user) {
+      this.loading = true;
+
+      const method = user.id ? 'put' : 'post'
+
+      api[method]('/', user).then(() => {
+
+        Swal.fire(
+          'Usuário cadastrado com sucesso!',
+          '',
+          'success'
+        )
+
+        this.user = { gender: '' }
+      }).catch(() => {
+        Swal.fire({
+          type: 'error',
+          text: 'Erro ao cadastrar o usuário',
+        })
+
+      }).finally(() => {
+        this.loading = false;
+      })
+    },
+    fillUserInfo (user) {
+      this.user = user;
+    },
+    deleteUser (id) {
+      Swal.fire({
+        title: 'Exclusão de usuário',
+        text: 'Tem certeza que quer excluir o usuário.',
+        type: 'warning',
+        confirmButtonText: 'Confirmar',
+        showCancelButton: true,
+        cancelButtonText: 'Cancelar',
+      }).then(result => {
+
+        if (!result.value) return;
+
+        api.delete(`/${id}`).then(() => {
+          const userIndex = this.users.findIndex(user => user.id === id)
+          this.users.splice(userIndex, 1);
+
+          Swal.fire(
+            'Usuário excluído com sucesso!',
+            '',
+            'success'
+          )
+        }).catch(() => {
+          Swal.fire({
+            type: 'error',
+            title: 'Erro ao excluir o usuário!'
+          })
+        })
+      })
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
