6a58511360cd55e77d23ef0937f43db1259afc6e
Flavio Silva <flv.alves@gmail.com>
2019-11-05 20:11:51 -0300

    comment swagger code to avoid conflict

--- services/java-user-api/src/main/java/br/com/fiap/controller/PersonController.java
+++ services/java-user-api/src/main/java/br/com/fiap/controller/PersonController.java
@@ -2,62 +2,66 @@ package br.com.fiap.controller;
 
 
 import br.com.fiap.model.Person;
+import br.com.fiap.services.UserRegistrationSource;
 import br.com.fiap.services.PersonServices;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
-@Api(value = "Person Endpoint", tags = {"PersonEndpoint"})
+//@Api(value = "Person Endpoint", tags = {"PersonEndpoint"})
 @RestController
 @RequestMapping("/api/person/v1")
+@EnableBinding(UserRegistrationSource.class)
 public class PersonController {
 
     @Autowired
     private PersonServices services;
 
-    @ApiOperation(value = "Find All People Recorded.")
+//    @ApiOperation(value = "Find All People Recorded.")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<Person> findAll() {
         return services.findAll();
     }
 
-    @ApiOperation(value = "Find People By your Id.")
+//    @ApiOperation(value = "Find People By your Id.")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
     public Person findById(@PathVariable("id") Long id ) {
         return services.findById(id);
     }
 
-    @ApiOperation(value = "Create a People.")
+//    @ApiOperation(value = "Create a People.")
     @ResponseStatus(HttpStatus.OK)
     @PostMapping
-    public Person create(@RequestBody Person person) {
-        return services.create(person);
+    public Person create(@RequestBody Person newPerson) {
+        return services.create(newPerson);
     }
 
-    @ApiOperation(value = "Update People By your Id.")
+//    @ApiOperation(value = "Update People By your Id.")
     @ResponseStatus(HttpStatus.OK)
     @PutMapping
     public Person update(@RequestBody Person person) {
         return services.update(person);
     }
 
-    @ApiOperation(value = "Delete People by your Id.")
+//    @ApiOperation(value = "Delete People by your Id.")
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") Long id ) {
         services.delete(id);
         return ResponseEntity.ok().build();
     }
-
-
 }
 
 
